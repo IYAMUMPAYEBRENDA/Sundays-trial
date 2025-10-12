077b373813f31d01aba4adda54fbde879d10bdb4
IYAMUMPAYEBRENDA <iyamumpayebrenda@gmail.com>
2025-10-12 21:41:35 +0300

    Add files via upload

---
+++ sunday trial.js
@@ -0,0 +1,22 @@
+
+
+
+        
+        
+            Breakfast
+                7:00 - 9:30 AM
+            
+             class="menu-item">
+                Scrambled Eggs with Toast
+                class="price">$4.50
+            
+            class ="menu-item">
+                Oatmeal with Fruits
+                class="price">$3.50;
+    
+            class ="menu-item">
+                Pancakes with Maple Syrup;
+                 class="price">$5.00
+            
+        
+        
